b532bcfbf7fdf8d66a1dcaa8e56aa9440d140275
godssi <mincheol710313@gmail.com>
2024-09-20 17:41:16 +0900

    Merge humble-devel and example_program

--- dsr_example2/setup.py
+++ dsr_example2/setup.py
@@ -20,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'talker = dsr_example2.demo.publisher_member_function:main',
                 'dance = dsr_example2.demo.dance_m1013:main',
         ],
     },
